81635d15f60533f473633b065c6ed1fcb4813d54
catherio <catherio@mit.edu>
2018-08-15 23:38:37 +0000

    Skeleton of SPSA code (doesnt work yet)

--- unrestricted_advex/eval_kit/eval_with_attacks.py
+++ unrestricted_advex/eval_kit/eval_with_attacks.py
@@ -1,41 +1,38 @@
 """Evaluate a model with attacks."""
 
+from six.moves import xrange
+from cleverhans.model import Model
+from cleverhans.attacks import SPSA
 import numpy as np
+import tensorflow as tf
 
 
-def spsa_attack(model, batch_nchw, labels)::
+def make_cleverhans_model(model):
+    return None
+
+
+def spsa_attack(model, batch_nchw, labels, epsilon=(4. / 255)):
     with tf.Graph().as_default():
         # Prepare graph
-        x_input = tf.placeholder(tf.float32, shape=(1,) + batch_shape[1:])
+        x_input = tf.placeholder(tf.float32, shape=(1,) + batch_nchw.shape[1:])
         y_label = tf.placeholder(tf.int32, shape=(1,))
 
-        attack = SPSA(model)  # TODO cleverhansify
+        cleverhans_model = make_cleverhans_model(model)
+
+        attack = SPSA(cleverhans_model)
         x_adv = attack.generate(
             x_input, y=y_label, epsilon=epsilon, num_steps=30,
-            early_stop_loss_threshold=-1., batch_size=32, spsa_iters=16,
-            is_debug=True)
-
-        logits = model.get_logits(x_adv)
-        acc = _top_1_accuracy(logits, y_label)
+            early_stop_loss_threshold=-1., batch_size=batch_nchw.shape[0],
+            spsa_iters=16, is_debug=True)
 
         # Run computation
-        saver = tf.train.Saver(slim.get_model_variables())
-        session_creator = tf.train.ChiefSessionCreator(
-            scaffold=tf.train.Scaffold(saver=saver),
-            checkpoint_filename_with_path=FLAGS.checkpoint_path,
-            master=FLAGS.master)
-
-        num_correct = 0.
-        with tf.train.MonitoredSession(
-                session_creator=session_creator) as sess:
-            for i in xrange(num_images):
-                acc_val = sess.run(acc, feed_dict={
-                    x_input: np.expand_dims(images[i], axis=0),
+        with tf.Session() as sess:
+            for i in xrange(batch_nchw.shape[0]):
+                x_adv_np = sess.run(x_adv, feed_dict={
+                    x_input: np.expand_dims(batch_nchw[i], axis=0),
                     y_label: np.expand_dims(labels[i], axis=0),
                 })
-                tf.logging.info('Accuracy: %s', acc_val)
-                num_correct += acc_val
-            assert (num_correct / num_images) < 0.1
+        return x_adv_np
 
 
 def evaluate(model, data_iter, attacks=None, max_num_batches=1):
