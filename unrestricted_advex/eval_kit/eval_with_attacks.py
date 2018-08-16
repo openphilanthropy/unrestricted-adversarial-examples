Fix input normalization in data pipeline

unrestricted_advex/eval_kit/eval_with_attacks.py
@@ -1,60 +1,72 @@
 """Evaluate a model with attacks."""
 
-from six.moves import xrange
-from cleverhans.model import Model
-from cleverhans.attacks import SPSA
 import numpy as np
+import tcu_images
 import tensorflow as tf
 import torch
 import torchvision
-
-
-import tcu_images
+from cleverhans.attacks import SPSA
+from cleverhans.model import Model
+from six.moves import xrange
 
 
 class CleverhansModelWrapper(Model):
-    def __init__(self, model_fn):
-        """
-        Wrap a callable function that takes a numpy array of shape (N, C, H, W),
-        and outputs a numpy vector of length N, with each element in range [0, 1].
-        """
-        self.nb_classes = 2
-        def two_class_model_fn(x):
-            class_one_logit = model_fn(x)
-            class_zero_logit = -class_one_logit
-            stack = np.vstack([class_zero_logit, class_one_logit]).T
-            return stack
-        self.model_fn = two_class_model_fn
+  def __init__(self, model_fn):
+    """
+    Wrap a callable function that takes a numpy array of shape (N, C, H, W),
+    and outputs a numpy vector of length N, with each element in range [0, 1].
+    """
+    self.nb_classes = 2
 
-    def fprop(self, x, **kwargs):
-        logits_op = tf.py_func(self.model_fn, [x], tf.float32)
-        return {'logits': logits_op}
+    def two_class_model_fn(x):
+      class_one_logit = model_fn(x)
+      class_zero_logit = -class_one_logit
+      stack = np.vstack([class_zero_logit, class_one_logit]).T
+      return stack
+
+    self.model_fn = two_class_model_fn
+
+  def fprop(self, x, **kwargs):
+    logits_op = tf.py_func(self.model_fn, [x], tf.float32)
+    return {'logits': logits_op}
 
 
 def spsa_attack(model, batch_nchw, labels, epsilon=(4. / 255)):
-    with tf.Graph().as_default():
-        # Prepare graph
-        x_input = tf.placeholder(tf.float32, shape=(1,) + batch_nchw.shape[1:])
-        y_label = tf.placeholder(tf.int32, shape=(1,))
+  with tf.Graph().as_default():
+    # Prepare graph
+    x_input = tf.placeholder(tf.float32, shape=(1,) + batch_nchw.shape[1:])
+    y_label = tf.placeholder(tf.int32, shape=(1,))
 
-        cleverhans_model = CleverhansModelWrapper(model)
-        attack = SPSA(cleverhans_model)
-        x_adv = attack.generate(
-            x_input, y=y_label, epsilon=epsilon, num_steps=30,
-            early_stop_loss_threshold=-1., batch_size=batch_nchw.shape[0],
-            spsa_iters=16, is_debug=True)
+    cleverhans_model = CleverhansModelWrapper(model)
+    attack = SPSA(cleverhans_model)
+    x_adv = attack.generate(
+      x_input,
+      y=y_label,
+      epsilon=epsilon,
+      num_steps=30,
+      early_stop_loss_threshold=-1.,
+      batch_size=batch_nchw.shape[0],
+      spsa_iters=16,
+      is_debug=True)
 
-        # Run computation
-        with tf.Session() as sess:
-            for i in xrange(batch_nchw.shape[0]):
-                x_adv_np = sess.run(x_adv, feed_dict={
-                    x_input: np.expand_dims(batch_nchw[i], axis=0),
-                    y_label: np.expand_dims(labels[i], axis=0),
-                })
-        return x_adv_np
+    # Run computation
+    with tf.Session() as sess:
+      for i in xrange(batch_nchw.shape[0]):
+        x_adv_np = sess.run(x_adv, feed_dict={
+          x_input: np.expand_dims(batch_nchw[i], axis=0),
+          y_label: np.expand_dims(labels[i], axis=0),
+        })
+    return x_adv_np
 
 
 def evaluate(model, data_iter, attacks=None, max_num_batches=1):
+  """
+  :param model:
+  :param data_iter:
+  :param attacks:
+  :param max_num_batches:
+  :return: A list of tuples of (preds, labels) corredsponding to each attack
+  """
   if attacks is None:
     attacks = ['null']  # a single null attack
 
@@ -72,18 +84,15 @@ def evaluate(model, data_iter, attacks=None, max_num_batches=1):
         x_adv = attack_f(model, x_np, y_np)
 
       y_pred = model(x_adv)
-      y_pred = np.clip(y_pred, 0, 1)  # force into [0, 1]
 
       all_labels.append(y_np)
       preds_container.append(y_pred)
 
-  all_labels = np.concatenate(all_labels)
   all_preds = [np.concatenate(x) for x in all_preds]
+  return zip(*[all_labels, all_preds])
 
-  return all_labels, all_preds
 
-
-BIRD_CLASSES = list(range(80,100+1))
+BIRD_CLASSES = list(range(80, 100 + 1))
 BICYCLE_CLASSES = [671, 444]
 
 
@@ -91,15 +100,12 @@ def main():
   ### Get data
   data_dir = tcu_images.get_dataset('train')
 
-  normalize = torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                   std=[0.229, 0.224, 0.225])
-
   train_dataset = torchvision.datasets.ImageFolder(
     data_dir,
     torchvision.transforms.Compose([
       torchvision.transforms.Resize(224),
       torchvision.transforms.ToTensor(),
-      normalize,
+      lambda x: x / 255.,
     ]))
 
   dataset_iter = [(x.numpy(), y.numpy())
@@ -123,9 +129,14 @@ def main():
       return delta_logits.cpu().numpy()
 
   ### Evaluate attack
-  all_labels, all_preds = evaluate(model_fn, dataset_iter, attacks=[spsa_attack], max_num_batches=1)
-  correct = np.sum(np.equal(all_preds[0], all_labels).astype(np.float32)) / len(all_labels)
-  print(correct)
+  labels_and_preds = evaluate(model_fn, dataset_iter, attacks=['null', spsa_attack],
+                                   max_num_batches=1)
+
+  for labels, preds in labels_and_preds:
+    correct = np.equal(preds, labels).astype(np.float32)
+    correct_fracs = np.sum(correct, axis=0) / len(labels)
+    print(correct_fracs)
+
 
 if __name__ == '__main__':
   main()