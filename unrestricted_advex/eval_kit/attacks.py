69a5551a94d3510c9673c5a3b8f8af3fb44baa4a
Tom B Brown <nottombrown@gmail.com>
2018-08-20 16:27:51 -0700

    Add spatial attack (#1)
    * Create attacks.py file
    
    * Stub out spatial attack
    
    * WIP spatial attack
    
    * Broken fix
    
    * Use tf for xent
    
    * Fix NHWC woes to put tensorflow channels first
    
    * Add tests on imagenet

---
+++ unrestricted_advex/eval_kit/attacks.py
@@ -0,0 +1,180 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from itertools import product, repeat
+
+import numpy as np
+import tensorflow as tf
+from cleverhans.attacks import SPSA
+from cleverhans.model import Model
+from six.moves import xrange
+
+
+def np_sparse_softmax_cross_entropy_with_logits(
+        logits_np, labels_np, graph, sess):
+  with graph.as_default():
+    labels_tf = tf.placeholder(tf.int32)
+    logits_tf = tf.placeholder(tf.float32)
+    xent_tf = tf.nn.sparse_softmax_cross_entropy_with_logits(
+        labels=labels_tf, logits=logits_tf)
+    return sess.run(xent_tf, feed_dict={
+        labels_tf: labels_np, logits_tf: logits_np})
+
+
+class CleverhansModelWrapper(Model):
+  def __init__(self, model_fn):
+    """
+    Wrap a callable function that takes a numpy array of shape (N, C, H, W),
+    and outputs a numpy vector of length N, with each element in range [0, 1].
+    """
+    self.nb_classes = 2
+    self.model_fn = model_fn
+
+  def fprop(self, x, **kwargs):
+    logits_op = tf.py_func(self.model_fn, [x], tf.float32)
+    return {'logits': logits_op}
+
+
+def spsa_attack(model, x_np, y_np, epsilon=(16. / 255)):  # (4. / 255)):
+  with tf.Graph().as_default():
+    # Prepare graph
+    x_input = tf.placeholder(tf.float32, shape=(1,) + x_np.shape[1:])
+    y_label = tf.placeholder(tf.int32, shape=(1,))
+
+    cleverhans_model = CleverhansModelWrapper(model)
+    attack = SPSA(cleverhans_model)
+
+    x_adv = attack.generate(
+      x_input,
+      y=y_label,
+      epsilon=epsilon,
+      num_steps=200,
+      early_stop_loss_threshold=-1.,
+      spsa_samples=32,
+      is_debug=True)
+
+    # Run computation
+    all_x_adv_np = []
+    with tf.Session() as sess:
+      for i in xrange(len(x_np)):
+        x_adv_np = sess.run(x_adv, feed_dict={
+          x_input: np.expand_dims(x_np[i], axis=0),
+          y_label: np.expand_dims(y_np[i], axis=0),
+        })
+        all_x_adv_np.append(x_adv_np)
+    return np.concatenate(all_x_adv_np)
+
+
+def null_attack(model, x_np, y_np):
+  del model, y_np  # unused
+  return x_np
+
+
+def spatial_attack(model, x_np, y_np):
+  attack = SpatialGridAttack(model, image_shape_hwc=x_np.shape[1:],
+                            spatial_limits=[0, 0, 0],
+                             grid_granularity=[1, 1, 1],
+)
+  x_adv, transform_adv = attack.perturb_grid(x_input_np=x_np, y_input_np=y_np)
+  return x_adv
+
+
+class SpatialGridAttack:
+  def __init__(self, model, image_shape_hwc,
+               spatial_limits=[3, 3, 30],
+               grid_granularity=[5, 5, 31],
+               ):
+    """
+    :param model: a callable: batch-input -> batch-probability in [0, 1]
+    :param spatial_limits:
+    :param grid_granularity:
+    """
+    self.limits = spatial_limits
+    self.granularity = grid_granularity
+
+    # Construct graph for spatial attack
+
+    self.graph = tf.Graph()
+    with self.graph.as_default():
+      # self._x_for_trans = tf.placeholder(tf.float32, shape=[None] + IM_SHAPE)
+      self._x_for_trans = tf.placeholder(tf.float32)
+      self._t_for_trans = tf.placeholder(tf.float32, shape=[None, 3])
+      self._tranformed_x_op = apply_transformation(
+        self._x_for_trans,
+        self._t_for_trans,
+      image_height=image_shape_hwc[0],
+      image_width=image_shape_hwc[1],
+      )
+      self.session = tf.Session()
+
+    self.model = model
+    self.grid_store = []
+
+  def perturb_grid(self, x_input_np, y_input_np):
+    n = len(x_input_np)
+    grid = product(*list(np.linspace(-l, l, num=g)
+                         for l, g in zip(self.limits, self.granularity)))
+
+    worst_x = np.copy(x_input_np)
+    worst_t = np.zeros([n, 3])
+    max_xent = np.zeros(n)
+    all_correct = np.ones(n).astype(bool)
+
+    for horizontal_trans, vertical_trans, rotation in grid:
+      trans_np = np.stack(
+        repeat([horizontal_trans, vertical_trans, rotation], n))
+
+      # Apply the spatial attack
+      with self.graph.as_default():
+        x_np = self.session.run(self._tranformed_x_op, feed_dict={
+          self._x_for_trans: x_input_np,
+          self._t_for_trans: trans_np,
+
+        })
+      # See how the model performs on the perturbed input
+      logits = self.model(x_np)
+      preds = np.argmax(logits, axis=1)
+
+      cur_xent = np_sparse_softmax_cross_entropy_with_logits(
+        logits, y_input_np, self.graph, self.session)
+
+      cur_xent = np.asarray(cur_xent)
+      cur_correct = np.equal(y_input_np, preds)
+
+      # Select indices to update: we choose the misclassified transformation
+      # of maximum xent (or just highest xent if everything else if correct).
+      idx = (cur_xent > max_xent) & (cur_correct == all_correct)
+      idx = idx | (cur_correct < all_correct)
+      max_xent = np.maximum(cur_xent, max_xent)
+      all_correct = cur_correct & all_correct
+
+      idx = np.expand_dims(idx, axis=-1)  # shape (bsize, 1)
+      worst_t = np.where(idx, trans_np, worst_t)  # shape (bsize, 3)
+
+      idx = np.expand_dims(idx, axis=-1)
+      idx = np.expand_dims(idx, axis=-1)  # shape (bsize, 1, 1, 1)
+      worst_x = np.where(idx, x_np, worst_x, )  # shape (bsize, 32, 32, 3)
+
+    return worst_x, worst_t
+
+
+def apply_transformation(x, transform, image_height, image_width, pad_mode='CONSTANT'):
+  # Map a transformation onto the input
+
+
+  trans_x, trans_y, rot = tf.unstack(transform, axis=1)
+  rot *= np.pi / 180  # convert degrees to radians
+
+  x = tf.pad(x, [[0, 0], [16, 16], [16, 16], [0, 0]], pad_mode)
+
+  # rotate and translate image
+  ones = tf.ones(shape=tf.shape(trans_x))
+  zeros = tf.zeros(shape=tf.shape(trans_x))
+  trans = tf.stack([ones, zeros, -trans_x,
+                    zeros, ones, -trans_y,
+                    zeros, zeros], axis=1)
+  x = tf.contrib.image.rotate(x, rot, interpolation='BILINEAR')
+  x = tf.contrib.image.transform(x, trans, interpolation='BILINEAR')
+  return tf.image.resize_image_with_crop_or_pad(
+    x, image_height, image_width)
--- unrestricted_advex/eval_kit/tests/test_original_imagenet.py
+++ unrestricted_advex/eval_kit/tests/test_original_imagenet.py
@@ -6,11 +6,9 @@ from __future__ import print_function
 import os
 
 import numpy as np
-import tcu_images
 import tensorflow as tf
 import torch
 import torchvision
-from tcu_images import CLASS_NAME_TO_IMAGENET_CLASS
 from tensorflow.keras.applications.resnet50 import preprocess_input
 
 from unrestricted_advex.eval_kit import attacks
@@ -50,13 +48,14 @@ def save_image_to_png(image_np, filename):
   img.save(filename)
 
 
-def get_torch_tcu_dataset_iter(batch_size):
-  data_dir = tcu_images.get_dataset('train')
+def get_torch_imagenet_dataset_iter(batch_size):
+  data_dir = '/root/datasets/unpacked_imagenet_pytorch/val'
 
   train_dataset = torchvision.datasets.ImageFolder(
     data_dir,
     torchvision.transforms.Compose([
-      torchvision.transforms.Resize(224),
+      torchvision.transforms.Resize(256),
+      torchvision.transforms.CenterCrop(224),
       torchvision.transforms.ToTensor(),
       lambda x: torch.einsum('chw->hwc', [x]),
     ]))
@@ -65,39 +64,11 @@ def get_torch_tcu_dataset_iter(batch_size):
     train_dataset,
     batch_size=batch_size,
     shuffle=True)
-
-  assert train_dataset.class_to_idx['bicycle'] == 0
-  assert train_dataset.class_to_idx['bird'] == 1
-
-  dataset_iter = [(x.numpy(), y.numpy()) for (x, y) in iter(data_loader)]
+  dataset_iter = ((x.numpy(), y.numpy()) for (x, y) in iter(data_loader))
   return dataset_iter
 
 
-def get_torch_tcu_model():
-  print("WARNING: Torch model currently only gets 50% top1 accuracy and may have a preprocessing issue")
-
-  pytorch_model = torchvision.models.resnet50(pretrained=True)
-  pytorch_model = pytorch_model.cuda()
-  pytorch_model.eval()  # switch to eval mode
-
-  def model_fn(x_np):
-    with torch.no_grad():
-      x = torch.from_numpy(x_np).cuda()
-      x = torch.einsum('bhwc->bchw', [x])
-      logits1000 = pytorch_model(x)
-
-      bird_max_logit, _ = torch.max(
-        logits1000[:, CLASS_NAME_TO_IMAGENET_CLASS['bird']], dim=1)
-      bicycle_max_logit, _ = torch.max(
-        logits1000[:, CLASS_NAME_TO_IMAGENET_CLASS['bicycle']], dim=1)
-      logits = torch.cat((bicycle_max_logit[:, None],
-                          bird_max_logit[:, None]), dim=1)
-      return logits.cpu().numpy()
-
-  return model_fn
-
-
-def get_keras_tcu_model():
+def get_keras_imagenet_model():
   tf.keras.backend.set_image_data_format('channels_last')
 
   k_model = tf.keras.applications.resnet50.ResNet50(
@@ -107,22 +78,30 @@ def get_keras_tcu_model():
   def model_wrapper(x_np):
     # it seems keras pre-trained model directly output softmax-ed probs
     x_np = preprocess_input(x_np * 255)
-
     prob1000 = k_model.predict_on_batch(x_np) / 10
     fake_logits1000 = np.log(prob1000)
-
-    bird_max_logit = np.max(
-      fake_logits1000[:, CLASS_NAME_TO_IMAGENET_CLASS['bird']], axis=1)
-    bicycle_max_logit = np.max(
-      fake_logits1000[:, CLASS_NAME_TO_IMAGENET_CLASS['bicycle']], axis=1)
-    logits = np.concatenate((bicycle_max_logit[:, None],
-                             bird_max_logit[:, None]), axis=1)
-    return logits
+    return fake_logits1000
 
   return model_wrapper
 
 
-def evaluate_tcu_model(model_fn, dataset_iter):
+def get_torch_imagenet_model():
+  print("WARNING: Torch model currently only gets 50% top1 accuracy and may have a preprocessing issue")
+  pytorch_model = torchvision.models.resnet50(pretrained=True)
+  pytorch_model = pytorch_model.cuda()
+  pytorch_model.eval()  # switch to eval mode
+
+  def model_fn(x_np):
+    with torch.no_grad():
+      x = torch.from_numpy(x_np).cuda()
+      x = torch.einsum('bhwc->bchw', [x])
+      logits = pytorch_model(x)
+      return logits.cpu().numpy()
+
+  return model_fn
+
+
+def evaluate_imagenet_model(model_fn, dataset_iter):
   for (attack_fn, attack_name) in [
     (attacks.null_attack, 'null_attack'),
     (attacks.spatial_attack, 'spatial_attack'),
@@ -132,16 +111,18 @@ def evaluate_tcu_model(model_fn, dataset_iter):
     logits, labels = run_attack(
       model_fn, dataset_iter, attack_fn, max_num_batches=1,
       save_image_dir=os.path.join('/tmp/eval_with_attacks', attack_name))
-    preds = (logits[:, 0] < logits[:, 1]).astype(np.int64)
+
+    preds = np.argmax(logits, axis=1)
     correct = np.equal(preds, labels).astype(np.float32)
     correct_fracs = np.sum(correct, axis=0) / len(labels)
-    print("Fraction correct under %s: %.3f" % (attack_name, correct_fracs))
+    print("Top1 accuracy under %s: %.3f" % (attack_name, correct_fracs))
 
 
 def main():
-  tcu_dataset_iter = get_torch_tcu_dataset_iter(batch_size=4)
-  model_fn = get_keras_tcu_model()
-  evaluate_tcu_model(model_fn, tcu_dataset_iter)
+  tcu_dataset_iter = get_torch_imagenet_dataset_iter(batch_size=32)
+  # model_fn = get_torch_imagenet_model()
+  model_fn = get_keras_imagenet_model()
+  evaluate_imagenet_model(model_fn, tcu_dataset_iter)
 
 
 if __name__ == '__main__':
