ffe2f5fae253754f852607324b03ff84b708f910
nottombrown <nottombrown@gmail.com>
2018-08-20 10:52:20 -0700

    Broken fix

--- unrestricted_advex/eval_kit/attacks.py
+++ unrestricted_advex/eval_kit/attacks.py
@@ -14,7 +14,7 @@ from six.moves import xrange
 def softmax(x):
     """Compute softmax values for each sets of scores in x."""
     e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum(axis=1)
+    return e_x / e_x.sum(axis=1, keepdims=True)
 
 
 def log_softmax(x):
