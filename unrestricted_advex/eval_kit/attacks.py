37f4f6aa0d08f4ad06cfd8fb6fb4c41b00aca0b8
Nicholas Carlini <nicholas@carlini.com>
2018-08-22 20:32:04 +0000

    SPSA now works on MNIST

--- unrestricted_advex/eval_kit/attacks.py
+++ unrestricted_advex/eval_kit/attacks.py
@@ -169,7 +169,6 @@ class SpatialGridAttack:
 
       if self.valid_check is not None:
         is_valid = self.valid_check(x_downsize_np, x_np)
-        print(is_valid)
         cur_correct |= ~is_valid
         cur_xent -= is_valid*1e9
       
