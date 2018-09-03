77de033b0795e656bbcb20842d8bfb4b6137493e
nottombrown <nottombrown@gmail.com>
2018-09-02 17:51:28 -0700

    Remove image that was smaller than 299x299

--- bird-or-bicycle/bird_or_bicyle/metadata/__init__.py
+++ bird-or-bicycle/bird_or_bicyle/metadata/__init__.py
@@ -2,7 +2,7 @@
 SHASUMS = {
   '0.0.2': {
     'train': '902dc2652e46011688855a5ae33ab734971f4db4',
-    'test': '79fc78510d6b1174616a1f5d929830848b1e2933',
+    'test': '394bce590bbf0c64c7e304f1db707aa60219b1fd',
     'extras': '4d4e533d282e1bbac9c6e860b86d5f5e444b8775',
   }
 }
