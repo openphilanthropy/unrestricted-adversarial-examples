b4e3f61176800286c679e419d9d1611f9ca5bf56
nottombrown <nottombrown@gmail.com>
2018-09-12 16:42:23 -0700

    Small refactor of test_two_class_mnist

--- unrestricted-advex/unrestricted_advex/tests/test_two_class_mnist.py
+++ unrestricted-advex/unrestricted_advex/tests/test_two_class_mnist.py
@@ -1,12 +1,6 @@
-import itertools
-import os
-
 import numpy as np
-import tensorflow as tf
-
-from tensorflow.examples.tutorials.mnist import input_data
-from unrestricted_advex.mnist_baselines import mnist_convnet, mnist_utils
 from unrestricted_advex import attacks, eval_kit
+from unrestricted_advex.mnist_baselines import mnist_utils
 
 
 def test_two_class_mnist():
@@ -16,6 +10,7 @@ def test_two_class_mnist():
   train_batches = 20
 
   # Train a little MNIST classifier from scratch
+  print("Training mnist classifier...")
   mnist = mnist_utils.mnist_dataset(one_hot=False)
   num_datapoints = batch_size * dataset_total_n_batches
   next_batch_iter = mnist_utils.two_class_iter(
@@ -24,7 +19,7 @@ def test_two_class_mnist():
     label_scheme='one_hot', cycle=True)
 
   mnist_utils.train_mnist(model_dir, lambda: next(next_batch_iter),
-                          train_batches, "vanilla", save_every=(train_batches-1),
+                          train_batches, "vanilla", save_every=(train_batches - 1),
                           print_every=10)
 
   # Test it on small attacks *on the training set*
@@ -38,7 +33,7 @@ def test_two_class_mnist():
       grid_granularity=[4, 4, 4],
       black_border_size=4,
       valid_check=mnist_utils.mnist_valid_check),
-    }
+  }
 
   results = {}
   for (name, attack) in attack_list.items():
@@ -52,3 +47,7 @@ def test_two_class_mnist():
 
   assert results['null_attack'] >= (num_datapoints - 1)
   assert results['spatial_grid_attack'] <= num_datapoints * 0.7
+
+
+if __name__ == '__main__':
+  test_two_class_mnist()
