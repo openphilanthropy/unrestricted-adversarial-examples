bad93d1d1db310c32749d026e5128aa4f93e3df2
nottombrown <nottombrown@gmail.com>
2018-09-26 20:14:51 -0700

    Add basic speed test

---
+++ unrestricted-advex/unrestricted_advex/tests/test_undefended_resnet.py
@@ -0,0 +1,46 @@
+"""Test examples/undefended_pytorch.
+
+We only run the script for training / evaluation
+for one tiny batch to verify that the program can
+successfully run without issue. The correctness of
+the results are not checked in this auto-test.
+"""
+
+import bird_or_bicycle
+import pytest
+import torch
+from unrestricted_advex import attacks
+from unrestricted_advex.eval_kit import evaluate_two_class_unambiguous_model
+
+from examples.undefended_keras_resnet.main import undefended_keras_model_fn
+
+
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="Resnet tests require CUDA")
+def test_spatial_speed():
+  """
+
+  Original: 165.14s
+
+  :return:
+  """
+  bird_or_bicycle_shape = (224, 224, 3)
+  bird_or_bicycle_spatial_limits = [18, 18, 30]
+  bird_or_bicycle_black_border_size = 20
+
+  spatial_attack = attacks.SpatialGridAttack(
+    image_shape_hwc=bird_or_bicycle_shape,
+    spatial_limits=bird_or_bicycle_spatial_limits,
+    grid_granularity=[5, 5, 31],
+    black_border_size=bird_or_bicycle_black_border_size,
+  )
+  spatial_attack._stop_after_n_datapoints = 32
+
+  dataset_iter = bird_or_bicycle.get_iterator('train')
+  return evaluate_two_class_unambiguous_model(
+    undefended_keras_model_fn, dataset_iter,
+    model_name='undefended_keras_resnet',
+    attack_list=[spatial_attack])
+
+
+if __name__ == '__main__':
+  test_spatial_speed()
