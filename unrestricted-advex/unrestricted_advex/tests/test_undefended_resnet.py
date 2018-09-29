5c54535000779a6f3b2615bb42439089325a25f5
nottombrown <nottombrown@gmail.com>
2018-09-29 14:17:56 -0700

    Fix import error

--- unrestricted-advex/unrestricted_advex/tests/test_undefended_resnet.py
+++ unrestricted-advex/unrestricted_advex/tests/test_undefended_resnet.py
@@ -10,16 +10,47 @@ from __future__ import division
 from __future__ import print_function
 
 import bird_or_bicycle
+import numpy as np
 import pytest
+import tensorflow as tf
 import torch
+from bird_or_bicycle import CLASS_NAME_TO_IMAGENET_CLASS
+from tensorflow.keras.applications.resnet50 import preprocess_input
 from unrestricted_advex import attacks
 from unrestricted_advex.eval_kit import evaluate_two_class_unambiguous_model
-
-from examples.undefended_keras_resnet.main import undefended_keras_model_fn
 
 
 @pytest.mark.skipif(not torch.cuda.is_available(), reason="Resnet tests require CUDA")
 def test_spatial_speed():
+  # Variable scoping lets us use our _graph in our model_fn
+  _graph = tf.Graph()
+  with _graph.as_default():
+    k_model = tf.keras.applications.resnet50.ResNet50(
+      include_top=True, weights='imagenet', input_tensor=None,
+      input_shape=None, pooling=None, classes=1000)
+
+  def undefended_keras_model_fn(x_np):
+    """A normal keras resnet that was pretrained on ImageNet"""
+    x_np = preprocess_input(x_np * 255)
+
+    with _graph.as_default():
+      prob1000 = k_model.predict_on_batch(x_np) / 10
+
+    # Keras returns softmax-ed probs, we convert them back to logits
+    fake_logits1000 = np.log(prob1000)
+
+    bird_max_logit = np.max(
+      fake_logits1000[:, CLASS_NAME_TO_IMAGENET_CLASS['bird']], axis=1)
+    bicycle_max_logit = np.max(
+      fake_logits1000[:, CLASS_NAME_TO_IMAGENET_CLASS['bicycle']], axis=1)
+
+    two_class_logits = np.concatenate((
+      bicycle_max_logit[:, None],
+      bird_max_logit[:, None]),
+      axis=1)
+    return two_class_logits
+
+  # Set up standard attack params
   bird_or_bicycle_shape = (224, 224, 3)
   bird_or_bicycle_spatial_limits = [18, 18, 30]
   bird_or_bicycle_black_border_size = 20
