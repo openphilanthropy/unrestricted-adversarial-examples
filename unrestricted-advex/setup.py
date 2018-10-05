dbc6e40c6782cf8020c1a824d01a2593411615c9
nottombrown <nottombrown@gmail.com>
2018-10-04 17:53:08 -0700

    add scikit image

--- unrestricted-advex/setup.py
+++ unrestricted-advex/setup.py
@@ -17,6 +17,7 @@ setup(
     'randomgen',
     'terminaltables ~= 3.1',
     'imagenet_c ~= 0.0.2',
+    'scikit-image ~= 0.13'
   ],
   # Explicit dependence on TensorFlow is not supported.
   # See https://github.com/tensorflow/tensorflow/issues/7166
