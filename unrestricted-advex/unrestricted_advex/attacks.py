147854e8b64961281424629076f6013fc9fbb786
nottombrown <nottombrown@gmail.com>
2018-10-05 09:30:48 -0700

    Debug mode

--- unrestricted-advex/unrestricted_advex/attacks.py
+++ unrestricted-advex/unrestricted_advex/attacks.py
@@ -132,7 +132,8 @@ class CommonCorruptionsAttack(Attack):
     for batch_idx, x in enumerate(images_batch_nhwc):
       corrupt_args = [(x, corruption_name, self.severity)
                       for corruption_name in self.corruption_names]
-      corrupt_x_batch = self.pool.map(_corrupt_float32_image_star, corrupt_args)
+      print("WARNING: in DEBUG mode - Running single threaded for better errors")
+      corrupt_x_batch = map(_corrupt_float32_image_star, corrupt_args)
       logits_batch = model_fn(np.array(corrupt_x_batch))
       label = y_np[batch_idx]
 
