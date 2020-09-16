e10b7a47e673490ca633f06211347574e9779592
Nicholas Carlini <nicholas@carlini.com>
2020-09-15 23:57:27 -0700

    Remove bird or bicycle dataset; moved to new repository

--- bird-or-bicycle/bird_or_bicycle/__init__.py
+++
@@ -1,8 +0,0 @@
-from bird_or_bicycle.dataset import (
-  CLASS_NAME_TO_IMAGENET_CLASS,
-  BIRD_IDX,
-  BICYCLE_IDX,
-  verify_dataset_integrity,
-  get_dataset,
-)
-from bird_or_bicycle.iterator import get_iterator
